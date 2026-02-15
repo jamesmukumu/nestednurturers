91eaf4460c9366289f773e96c12dfe13f6c5969b
jamesmukumu <jamesmukumu03@gmail.com>
2026-02-15 09:00:26 +0300

    appendex

--- resources/js/comps/Header.jsx
+++ resources/js/comps/Header.jsx
@@ -180,7 +180,7 @@ export function Header() {
   
 
     <a href="/">
-      <img className="w-auto h-7" src="images/logo.webp" alt="Logo"/>
+      <img className="w-auto h-7" src="images/updates-logo.jpeg" alt="Logo"/>
     </a>
   
     <div class="flex flex-col justify-between flex-1 mt-6">
